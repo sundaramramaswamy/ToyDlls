efe85a1caf666ffd15026d9d0e4ae1d9fb1a5bec
Sundaram Ramaswamy <suramaswamy@microsoft.com>
2026-01-30 09:24:01 +0530

    Split implementation into two files
    Drop the needless exports (copy-pasta).

---
+++ Static/src/add.cpp
@@ -0,0 +1,5 @@
+#include "StaticMath.h"
+
+int add(int a, int b) {
+  return a + b;
+}
--- Static/src/sub.cpp
+++ Static/src/sub.cpp
@@ -1,9 +1,5 @@
 #include "StaticMath.h"
 
-int add(int a, int b) {
-  return a + b;
-}
-
 int sub(int a, int b) {
   return a - b;
 }
